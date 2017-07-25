f3145c66e39b61974e2cc898cca49d00388a5a35
david-mansen <davidmansen@gmail.com>
2017-07-25 13:59:14 -0400

    Move from plain html/css

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container">
+      &copy; Copyright 2016 Your Name
+    </div>
+  </footer>
---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<header id="masthead">
+    <div class="container">
+      <a href="index.html" id="logo">Your Name</a>
+      <nav>
+        <ul>
+          <li><a href="index.php">About</a></li>
+          <li><a href="projects.php">Portfolio</a></li>
+          <li><a href="about.php">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
